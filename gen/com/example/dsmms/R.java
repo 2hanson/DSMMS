7129305a6abeccc0443d0130ac80c6fb7b7f2654
hanson <hangzhong.yu@gmail.com>
2012-10-18 16:23:06 +0800

    finish ctrl

--- gen/com/example/dsmms/R.java
+++ gen/com/example/dsmms/R.java
@@ -30,14 +30,17 @@ public final class R {
         public static final int settings=0x7f02000b;
     }
     public static final class id {
-        public static final int ctrl_textview=0x7f080000;
-        public static final int ctrlbutton=0x7f080004;
-        public static final int menu_settings=0x7f080007;
-        public static final int settings=0x7f080006;
-        public static final int startbutton=0x7f080002;
-        public static final int stopbutton=0x7f080003;
-        public static final int testbutton=0x7f080001;
-        public static final int title=0x7f080005;
+        public static final int autoTurnButton=0x7f080000;
+        public static final int ctrl_textview=0x7f080006;
+        public static final int ctrlbutton=0x7f080007;
+        public static final int currentAPView=0x7f080001;
+        public static final int currentIPV4View=0x7f080002;
+        public static final int currentIPV6View=0x7f080003;
+        public static final int menu_settings=0x7f08000a;
+        public static final int settings=0x7f080009;
+        public static final int startbutton=0x7f080004;
+        public static final int stopbutton=0x7f080005;
+        public static final int title=0x7f080008;
     }
     public static final class layout {
         public static final int activity_ctrl=0x7f030000;
@@ -49,12 +52,15 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
-        public static final int app_title=0x7f050009;
-        public static final int click_text=0x7f05000a;
+        public static final int app_title=0x7f050008;
+        public static final int auto_turn_text=0x7f05000a;
         public static final int ctrl_message=0x7f050006;
         public static final int ctrl_name=0x7f050001;
-        public static final int ctrl_text=0x7f050008;
-        public static final int default_script=0x7f05000b;
+        public static final int ctrl_text=0x7f050009;
+        public static final int currentAP=0x7f05000b;
+        public static final int currentIPV4=0x7f05000c;
+        public static final int currentIPV6=0x7f05000d;
+        public static final int default_script=0x7f05000e;
         public static final int hello_world=0x7f050002;
         public static final int menu_settings=0x7f050003;
         public static final int start_message=0x7f050005;
--- src/com/example/dsmms/CtrlActivity.java
+++ src/com/example/dsmms/CtrlActivity.java
@@ -1,6 +1,8 @@
 package com.example.dsmms;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -13,7 +15,7 @@ import android.widget.Button;
 public class CtrlActivity extends Activity {
 
 	private Process process;
-	private Button testButton;
+	private Button autoTurnButton;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,9 +27,9 @@ public class CtrlActivity extends Activity {
         
         setContentView(R.layout.activity_ctrl);
         
-        this.testButton = (Button)findViewById(R.id.testbutton);
+        this.autoTurnButton = (Button)findViewById(R.id.autoTurnButton);
         
-        this.testButton.setOnClickListener(onTestButtonClick);
+        this.autoTurnButton.setOnClickListener(onTestButtonClick);
 	}
 	
     private OnClickListener onTestButtonClick = new OnClickListener() {
@@ -67,25 +69,81 @@ public class CtrlActivity extends Activity {
     		this.process = null;
     	}
 
-    	Runtime runtime = Runtime.getRuntime();
+    	//Runtime runtime = Runtime.getRuntime();
+    	//this.process = runtime.exec(new String[]{ "/system/xbin/su", "-c", command});
     	
-    	this.process = runtime.exec(new String[]{ "/system/xbin/su", "-c", command});
-    	
+    	runtimeEXEC(new String[]{ "/system/xbin/su", "-c", command}, false);
     	//OutputStream stdin = null;
-    	
     	//stdin = this.process.getOutputStream();
-    	
     	//String line = "help" + "\n";
     	//stdin.write( line.getBytes() );
     	//stdin.flush();
-    	
     	//stdin.close();
-    	
+    	/*
     	StreamGobbler errorGobbler = new StreamGobbler(this.process.getErrorStream(), "Error");
     	StreamGobbler stdoutGobbler = new StreamGobbler(this.process.getInputStream(), "Output");
     	errorGobbler.start();
-    	stdoutGobbler.start();
+    	stdoutGobbler.start();*/
     }
+    
+
+    private void runtimeEXEC(String[] command, boolean handleOutput) throws IOException, InterruptedException
+    {
+    	if (this.process != null)
+    	{
+    		this.process.destroy();
+    	}
+    	
+    	Runtime runtime = Runtime.getRuntime();
+    	
+    	this.process = runtime.exec(command);
+    	
+    	BufferedReader err = new BufferedReader(
+		        new InputStreamReader(this.process.getErrorStream()));
+    	String lineErr = null;
+    	while ((lineErr = err.readLine()) != null) {
+    	}
+    	
+    	BufferedReader in = new BufferedReader(
+    		        new InputStreamReader(this.process.getInputStream()));
+    	String line = null;
+    	boolean skip = true;
+    	while ((line = in.readLine()) != null) {
+    		if (handleOutput == false)
+    		{
+    			continue;
+    		}
+    		//ine += line + "\n";
+    		if (skip == true)
+    		{
+    			skip = false;
+    		}
+    		else
+    		{
+    			String pid = "";
+    			for (int index = 0; index < line.length(); ++index)
+    			{
+    				char ch = line.charAt(index);
+    				//if ()
+    				if (Character.isDigit(ch))
+    				{
+    					while (Character.isDigit( ch = line.charAt(index) ))
+    					{
+        					pid = pid + ch;
+        					++index;
+    					}
+        				break;
+    				}
+    			}
+    			
+    			if (pid != null && pid != "")
+    			{
+    				runtimeEXEC(new String[]{ "/system/xbin/su", "-c", "kill", pid}, false);
+    			}
+    		}
+    	}
+    }
+    
     
     private void stopCommand()
     {
